501829b4008ea35c47673920956e1767e7629bc4
Infinity922 <ethanmcghan7@gmail.com>
2017-09-15 21:18:46 -0600

    Need to create branch

--- src/simplecalculatorv2/SimpleCalculatorv2.java
+++ src/simplecalculatorv2/SimpleCalculatorv2.java
@@ -5,6 +5,8 @@
  */
 package simplecalculatorv2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ethan
@@ -14,8 +16,21 @@ public class SimpleCalculatorv2 {
     /**
      * @param args the command line arguments
      */
+    public int add(){
+        int answer = 0;
+        return answer;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, Welcome to Calculator");
+        System.out.println("Do you want to 1 Add 2 Subtract 3 Multiply or 4 Divide");
+        int response = scanner.nextInt();
+        switch (response){
+            case 1:
+                
+        }
     }
     
 }
